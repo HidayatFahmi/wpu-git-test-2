73fc1b2e08c4cd71e1ca678c7a74b60cd65b4338
hidayatfahmi <hidayat.fh22@gmail.com>
2022-11-19 14:25:13 +0700

    Menambahkan lorems

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Sukses</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Excepturi nesciunt rem est id dicta tempore alias beatae, veniam, molestiae veritatis, doloribus iste aspernatur quidem similique esse rerum nihil repellat voluptate?</p>
 </body>
 </html>
